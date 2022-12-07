9d423e67acc75a32ece0ef9e989fd75cc7ecae86
Thiago Vitorino <thiagobrasilia@gmail.com>
2022-12-07 16:49:46 -0300

    Basic Queue operations

--- Queue/Queue.test.ts
+++ Queue/Queue.test.ts
@@ -2,13 +2,74 @@ import Queue from "./Queue"
 
 describe("Linked List", () => {
 
+	let q:Queue<number> = Queue()
+		
 	it("Should produce a Queue", () => {
 
-		let ll:Queue<number> = Queue()
-		expect(ll).toBeDefined()
-		expect(ll.head).not.toBeDefined()
-		expect(ll.tail).not.toBeDefined()
+		expect(q.size).toBeInstanceOf(Function)
+		expect(q.front).toBeInstanceOf(Function)
+		expect(q.rear).toBeInstanceOf(Function)
+		expect(q.enqueue).toBeInstanceOf(Function)
+		expect(q.dequeue).toBeInstanceOf(Function)
 
+		expect(q.size()).toBe(0)
+		expect(q.front()).toBe(undefined)
+		expect(q.front()).toBe(undefined)
+
+	})
+
+	it("Adds an element", () => {
+
+		q.enqueue(1)
+		expect(q.size()).toBe(1)
+		expect(q.front()).toBe(1)
+		expect(q.rear()).toBe(1)
+		
+	})
+
+	it("Adds a second element", () => {
+
+		q.enqueue(2)
+		expect(q.size()).toBe(2)
+		expect(q.front()).toBe(1)
+		expect(q.rear()).toBe(2)
+		
+	})
+
+	it("Creates an Array containing all elements of the Queue", () => {
+
+		let a = q.to_array()
+		expect(a.length).toBe(2)
+		expect(a[0]).toBe(1)
+		expect(a[1]).toBe(2)
+		
+	})
+
+	it("Removes an element", () => {
+
+		expect(q.dequeue()).toBe(1)
+		expect(q.size()).toBe(1)
+		expect(q.front()).toBe(2)
+		expect(q.rear()).toBe(2)
+		
+	})
+
+	it("Removes the last element", () => {
+
+		expect(q.dequeue()).toBe(2)
+		expect(q.size()).toBe(0)
+		expect(q.front()).toBe(undefined)
+		expect(q.rear()).toBe(undefined)
+		
+	})
+	
+	it("Attempt to remove from empty list", () => {
+
+		expect(q.dequeue()).toBe(undefined)
+		expect(q.size()).toBe(0)
+		expect(q.front()).toBe(undefined)
+		expect(q.rear()).toBe(undefined)
+		
 	})
 
 })
--- Queue/Queue.ts
+++ Queue/Queue.ts
@@ -1,38 +1,96 @@
 import { Option } from "../SharedTypes/Option"
 
+/**
+ * Returns the number of elements in this Queue
+ */
+type Size = () => number 
+
+/**
+ * Returns, but does not remove, the front element
+ */
+type Front<T> = () => T
+
+/**
+ * Returns, but does not remove, the rear Elemment
+ */
+type Rear<T> = () => T
+
+/**
+ * Adds an element to the end of our Qeue
+ * @param val The element to be added
+ * @returns {Queue} the Queue
+ */
+type Enqueue<T> = (val:T) => Queue<T> 
+
+/**
+ * Removes an element from the front of the Queue
+ * @returns {T} the dequeued element
+ */
+type Dequeue<T> = () => Option<T> 
+
+/**
+ * Returns an array listing all elements
+ * present in the Queue in order
+ * @returns {T[]} Array containing the elements of this Queue
+ */
+ type ToArray<T> = () => T[]
+
+/**
+ * A First In First Out Queue
+ */
 type Queue<T> = {
-	front: () => Option<T>,
-	rear: () => Option<T>,
-	size: () => number,
-	enqueue: (val:T) => Queue<T>
-	dequeue: () => Option<T>
+	front: Front<T>,
+	rear: Rear<T>,
+	size: Size,
+	enqueue: Enqueue<T>
+	dequeue: Dequeue<T>
+	to_array: ToArray<T>
 }
 
 const Queue = <T>():Queue<T> => {
 
-	const data 	= <T>[]
-	let idx_f  	= -1
-	let idx_r	= -1
+	const data 	= Array<T>()
+	let idx_f  	= 0
+	let idx_r	= 0
 
 	const size 	= () => idx_r-idx_f
-	const front	= () => data[idx_f]
-	const rear	= () => data[idx_r]
+	const front = () => data[idx_f]
+	const rear	= () => data[idx_r-1]
 
 	const enqueue = (val:T):Queue<T> => {
-		data[++idx_r] = val
+		data[idx_r] = val
+		idx_r++
 		return returnQueue()
+	} 
+
+	const dequeue = ():Option<T> => {
+		let el:Option<T>
+		if(idx_f < idx_r){
+			el = data[idx_f] as Option<T>
+			delete data[idx_f]
+			idx_f++
+		}
+		return el
 	}
 
-	const dequeue = ():Option<T> => data[idx_f--]
+	const to_array = ():T[] => 
+		data.slice(0)
+	
+	const from_array = ():T[] => 
+		data.slice(0)
 	
 	const returnQueue = ():Queue<T> => ({
 		size,
 		front,
 		rear,
 		enqueue,
-		dequeue
+		dequeue,
+		to_array
 	})
 
 	return returnQueue()
 
 }
+
+
+export default Queue

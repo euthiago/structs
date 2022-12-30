11783ef7ff3fd888db0dcfe4f00472f280e96d25
Thiago Vitorino <thiagobrasilia@gmail.com>
2022-12-30 14:53:54 -0300

    min and max heap extra info

--- Heap/MaxHeap.ts
+++ Heap/MaxHeap.ts
@@ -7,7 +7,7 @@ type MaxHeap<T> = Heap<T>
  * MaxHeap Data Structure
  * A Heap whose nodes have values bigger 
  * then their children's
- * Check Heap for more info
+ * Equivalent to Heap using Ge comparator
  * @param {Array<T>} arr initial values to populate our Heap
  */
 const MaxHeap = <T>(arr?:Array<T>):Heap<T> => 
--- Heap/MinHeap.ts
+++ Heap/MinHeap.ts
@@ -7,7 +7,7 @@ type MinHeap<T> = Heap<T>
  * MinHeap Data Structure
  * A Heap whose nodes have values smaller 
  * then their children's
- * Check Heap for more info
+ * Equivalent to Heap using Le comparator
  * @param {Array<T>} arr initial values to populate our Heap
  */
 const MinHeap = <T>(arr?:Array<T>):Heap<T> => 
